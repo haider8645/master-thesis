1ca362f56c253dc8c43d0f21b21df5fa1bb605c4
haider8645 <haider_lodhi@hotmail.com>
2018-01-26 20:52:49 +0100

    some changes to streamline the work
    Signed-off-by: haider8645 <haider_lodhi@hotmail.com>

--- python scripts/visualize3.py
+++ python scripts/visualize3.py
@@ -8,7 +8,7 @@ import cv2
 # display plots in this notebook
 #from IPython import get_ipython
 #get_ipython().run_line_magic('matplotlib', 'inline')
-caffe_root = '/home/haider/caffe/' # The caffe_root is changed to reflect the actual folder in the server.
+caffe_root = '/home/lod/master-thesis/' # The caffe_root is changed to reflect the actual folder in the server.
 sys.path.insert(0, caffe_root + 'python') # Correct the python path
 import caffe
 #matplotlib inline
@@ -21,8 +21,8 @@ caffe.set_device(0)
 caffe.set_mode_gpu()
 # set the model definitions since we are using a pretrained network here.
 # this protoype definitions can be changed to make significant changes in the learning method.
-model_def = '/home/haider/caffe/examples/master-thesis/train-mnistCAE12sym0302.prototxt'
-model_weights = '/home/haider/caffe/examples/master-thesis/snapshots_iter_19000.caffemodel'
+model_def = '/home/lod/master-thesis/examples/master-thesis/train-mnistCAE12sym0302.prototxt'
+model_weights = '/home/lod/master-thesis/examples/master-thesis/snapshots_iter_19000.caffemodel'
 
 
 net = caffe.Net(model_def,      # defines the structure of the model
@@ -39,13 +39,13 @@ net = caffe.Net(model_def,      # defines the structure of the model
 #img_blobinp = img[np.newaxis, np.newaxis, :, :]
 #net.blobs['data'].reshape(*img_blobinp.shape)
 #net.blobs['data'].data[...] = img_blobinp
-dirname = '/home/haider/caffe/output_cae02'
+dirname = '/home/lod/master-thesis/graphs/output_cae02'
 #os.mkdir(dirname)
-for j in range(15):
+for j in range(10):
 #os.path.join(dirname, face_file_name)
     net.forward()
     for i in range(1):
-        cv2.imwrite(os.path.join(dirname,'image_image_' + str(j) + '.jpg'), 255*net.blobs['data'].data[0,i])
+        cv2.imwrite(os.path.join(dirname,'input_image_' + str(j) + '.jpg'), 255*net.blobs['data'].data[0,i])
 
     for i in range(1):
         cv2.imwrite(os.path.join(dirname,'output_image_' + str(j) + '.jpg'), 255*net.blobs['deconv1neur'].data[0,i])
